8a1f2f05177e14793a9d053ad4162aa03e58dd9e
holly.mae.rogers@gmail.com <holly.mae.rogers@gmail.com>
2018-05-09 10:02:38 +0100

    minor changes

--- src/components/reactTasks/videos.js
+++ src/components/reactTasks/videos.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import Video from './video'
+import Video from './video' 
 
 export default class Videos extends React.Component {
   render() {
-    return <div> _.map(this.props,(video, i) => <Video key={i} video={video}/>)}</div>
+  	return 
+
+  	<div> 
+  	_.map(this.props.uploads, (video, i) => <Video key={i} video={video} />)
+
+  	</div>
   }
 }
