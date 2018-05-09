7e8d3b1b317fd0f7159376641010aa5d05e42472
holly.mae.rogers@gmail.com <holly.mae.rogers@gmail.com>
2018-05-09 15:24:21 +0100

    map change

--- src/components/reactTasks/videos.js
+++ src/components/reactTasks/videos.js
@@ -3,8 +3,7 @@ import Video from './video'
 
 export default class Videos extends React.Component {
   render() {
-  	const upload = _.map(this.props.uploads, (video, i) => <Video key={i} video={video} />)
-	return <div> {upload}  </div>
+	return <div> {_.map(this.props.uploads, (video, i) => <Video key={i} video={video} />)}  </div>
   }
 }
 
